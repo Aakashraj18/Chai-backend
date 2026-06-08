0c9d5dfc9577ae507e68af37685cd8897636e050
Aakash Kumar Rajak <akashkrrajak18@gmail.com>
2026-06-08 15:13:01 +0530

    user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String,  // cloudinary url
+        required: true,
+    },
+    thumbnail: {
+        type:string,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,   // cloudinary returns duration in seconds
+        required: true,
+    },
+    views: {
+        type:Number,
+        default: 0,
+    },
+    isPublished: {
+        type: Boolean,
+        default: true,
+    },
+    owner: {
+        type: schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, {timestamps: true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+
+export const Video = mongoose.model("Video", videoSchema)
