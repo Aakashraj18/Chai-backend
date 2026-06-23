80cb544bf65795572e6b8b938e212180bbc6207b
Aakash Kumar Rajak <akashkrrajak18@gmail.com>
2026-06-23 19:38:57 +0530

    Add getVideoById fun code in video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -2,6 +2,9 @@ import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 import { uploadOnCloudinary } from "../utils/cloudinary";
+import { User } from "../models/user.model.js"
+import { isValidObjectId } from "mongoose";
+import { Video } from "../models/video.model.js"
 
 const getAllvideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query,sortBy, sortType, userId } = req.query
@@ -54,4 +57,29 @@ const getAllvideos = asyncHandler(async (req, res) => {
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const {videoId} = req.params
+})
+
+
+const getVideoById = asyncHandler(async ( req, res) => {
+    const { videoId } = req.params
+
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid video id");
+    }
+
+    const video = await Video.findById(videoId);
+    
+    if(!video){
+        throw new ApiError(404,"video not found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            video,
+            "video fetched successfully"
+        )
+    );
 })
